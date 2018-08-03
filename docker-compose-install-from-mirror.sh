58c47a9c3ea210a5fb819564910fcd02b51c7384
sh777 <withoutbug@gmail.com>
2018-08-03 16:06:45 +0800

    Create docker-compose-install-from-mirror.sh

---
+++ docker-compose-install-from-mirror.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COMPOSE_VERSION=1.22.0
+sudo sh -c "curl -L http://hyf-sw.oss-cn-beijing.aliyuncs.com/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
